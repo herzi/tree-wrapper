7f1eea4a31a342e4b0711ec80c751cd6d3aef85c
Sven Herzberg <sven@imendio.com>
2007-09-08 20:29:30 +0200

    Added a tree model and text

--- main.c
+++ main.c
@@ -25,10 +25,37 @@
 
 #include <gtk/gtk.h>
 
+enum {
+	COL_TEXT,
+	N_COLUMNS
+};
+
+static GtkTreeModel*
+treemodel_new (void)
+{
+	GtkTreeStore* result;
+	GtkTreeIter   iter;
+
+	// text taken from http://209.85.135.104/search?q=cache:5GSj55F5I_YJ:www.newmediadesigner.de/38.htm+site:www.newmediadesigner.de+blindtext+archiv&hl=de&gl=de&strip=1
+	result = gtk_tree_store_new (N_COLUMNS, G_TYPE_STRING);
+	gtk_tree_store_append (result, &iter, NULL);
+	gtk_tree_store_set    (result, &iter,
+			       COL_TEXT, "This, of course, is not the real copy for this advertisement. The real words will be written once you have approved the headline. Rest assured, the words will expand the concept. With clarity. Conviction. And even a little wit. Because in today’s competitive marketing environment, the body copy of your advertisment must lead the reader through a series of disarmingly simple thoughts. All your supporting arguments must be communicated with simplicity and charm. And in such a way that the reader will read on. (After all, that’s a reader’s job: to read, isn’t it?) And by the time your readers have reached this point in the finished copy, you will have convinced them that you not only respect their intelligence, but you also understand their needs as consumers. As a result of which, your advertisment will repay your efforts. Take your sales; simply put, they will rise. Likewise your credibility. There’s every chance your competitors will wish they’d placed this advertisement, not you. While your customers will have probably forgotten that your competitors even exist. Which brings us, by a somewhat circuitous route, to another small point, but one which we feel should be raised. As a marketer, you probably don’t even believe in body copy. Let alone long body copy. (Unless you have a long body yourself.) Well, truth is, who‘s to blame you? Fact is, too much long body copy is dotted with such indulgent little phrases like truth is, fact is, and who’s to blame you. Trust us: we guarantee, with a hand over our heart, that no such indulgent rubbish will appear in your advertisement. That’s why God gave us big blue pencils. So we can expunge every expedient example of low-witted waffle. For you, the skies will be blue, the birds will sing, and your copy will be crafted by a dedicated little man whose wife will be sitting at home, knitting, wondering why your advertisement demands more of her husband‘s time than it should. But you will know why, won‘t you? You will have given her husband a chance to immortalise himself in print, writing some of the most persuasive prose on behalf of a truly enlightened purveyor of widgets. And so, while your dedicated reader, enslaved to each mellifluous paragraph, clutches his newspaper with increasing interest and intention to purchase, you can count all your increased profits and take pots of money to your bank. Sadly, this is not the real copy for this advertisement. But it could well be. All you have to do is look at the account executive sitting across your desk (the fellow with the lugubrious face and the calf-like eyes), and say ”Yes! Yes! Yes!“ And anything you want, body copy, dinners, women, will be yours. Couldn’t be fairer than that, could we?",
+			       -1);
+
+	return GTK_TREE_MODEL (result);
+}
+
 static GtkWidget*
 treeview_new (void)
 {
-	GtkWidget* result = gtk_tree_view_new ();
+	GtkTreeModel* model;
+	GtkWidget* result;
+
+	model  = treemodel_new ();
+	result = gtk_tree_view_new_with_model (model);
+	g_object_unref (model);
+
 	return result;
 }
 
