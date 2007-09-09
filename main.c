eadff6f7166e628bbeb0018b43ef4f5bb81a34c4
Sven Herzberg <sven@imendio.com>
2007-09-09 02:11:49 +0200

    My final version
    	* added more rows to be able to drag and drop stuff
    	* enabled reordering in the tree
    	* removed debug statement
    	* cleaned up the code a bit

--- main.c
+++ main.c
@@ -46,22 +46,17 @@ tree_cell_data_func (GtkTreeViewColumn* column,
 			    -1);
 
 	if (GTK_WIDGET_REALIZED (data)) {
-		GtkTreePath * path;
-		path = gtk_tree_model_get_path (model, iter);
+		GtkTreePath * path = gtk_tree_model_get_path (model, iter);
 		gtk_tree_view_get_cell_area (data,
 					     path,
 					     column,
 					     &rect);
 		gtk_tree_path_free (path);
-
-		g_print ("%d\n", rect.width);
-	} else {
-		rect.width = 100;
 	}
 
 	g_object_set (renderer,
 		      "text", text,
-		      "wrap-width", rect.width - 6, // FIXME: Kris, what's this number? Where does it come from?
+		      "wrap-width", MAX (100, rect.width) - 6, // FIXME: Kris, what's this number? Where does it come from?
 		      NULL);
 
 	g_free (text);
@@ -77,7 +72,23 @@ treemodel_new (void)
 	result = gtk_tree_store_new (N_COLUMNS, G_TYPE_STRING);
 	gtk_tree_store_append (result, &iter, NULL);
 	gtk_tree_store_set    (result, &iter,
-			       COL_TEXT, "This, of course, is not the real copy for this advertisement. The real words will be written once you have approved the headline. Rest assured, the words will expand the concept. With clarity. Conviction. And even a little wit. Because in today’s competitive marketing environment, the body copy of your advertisment must lead the reader through a series of disarmingly simple thoughts. All your supporting arguments must be communicated with simplicity and charm. And in such a way that the reader will read on. (After all, that’s a reader’s job: to read, isn’t it?) And by the time your readers have reached this point in the finished copy, you will have convinced them that you not only respect their intelligence, but you also understand their needs as consumers. As a result of which, your advertisment will repay your efforts. Take your sales; simply put, they will rise. Likewise your credibility. There’s every chance your competitors will wish they’d placed this advertisement, not you. While your customers will have probably forgotten that your competitors even exist. Which brings us, by a somewhat circuitous route, to another small point, but one which we feel should be raised. As a marketer, you probably don’t even believe in body copy. Let alone long body copy. (Unless you have a long body yourself.) Well, truth is, who‘s to blame you? Fact is, too much long body copy is dotted with such indulgent little phrases like truth is, fact is, and who’s to blame you. Trust us: we guarantee, with a hand over our heart, that no such indulgent rubbish will appear in your advertisement. That’s why God gave us big blue pencils. So we can expunge every expedient example of low-witted waffle. For you, the skies will be blue, the birds will sing, and your copy will be crafted by a dedicated little man whose wife will be sitting at home, knitting, wondering why your advertisement demands more of her husband‘s time than it should. But you will know why, won‘t you? You will have given her husband a chance to immortalise himself in print, writing some of the most persuasive prose on behalf of a truly enlightened purveyor of widgets. And so, while your dedicated reader, enslaved to each mellifluous paragraph, clutches his newspaper with increasing interest and intention to purchase, you can count all your increased profits and take pots of money to your bank. Sadly, this is not the real copy for this advertisement. But it could well be. All you have to do is look at the account executive sitting across your desk (the fellow with the lugubrious face and the calf-like eyes), and say ”Yes! Yes! Yes!“ And anything you want, body copy, dinners, women, will be yours. Couldn’t be fairer than that, could we?",
+			       COL_TEXT, "This, of course, is not the real copy for this advertisement.",
+			       -1);
+	gtk_tree_store_append (result, &iter, NULL);
+	gtk_tree_store_set    (result, &iter,
+			       COL_TEXT, "The real words will be written once you have approved the headline.",
+			       -1);
+	gtk_tree_store_append (result, &iter, NULL);
+	gtk_tree_store_set    (result, &iter,
+			       COL_TEXT, "Rest assured, the words will expand the concept.",
+			       -1);
+	gtk_tree_store_append (result, &iter, NULL);
+	gtk_tree_store_set    (result, &iter,
+			       COL_TEXT, "With clarity.",
+			       -1);
+	gtk_tree_store_append (result, &iter, NULL);
+	gtk_tree_store_set    (result, &iter,
+			       COL_TEXT, "Conviction. And even a little wit. Because in today’s competitive marketing environment, the body copy of your advertisment must lead the reader through a series of disarmingly simple thoughts. All your supporting arguments must be communicated with simplicity and charm. And in such a way that the reader will read on. (After all, that’s a reader’s job: to read, isn’t it?) And by the time your readers have reached this point in the finished copy, you will have convinced them that you not only respect their intelligence, but you also understand their needs as consumers. As a result of which, your advertisment will repay your efforts. Take your sales; simply put, they will rise. Likewise your credibility. There’s every chance your competitors will wish they’d placed this advertisement, not you. While your customers will have probably forgotten that your competitors even exist. Which brings us, by a somewhat circuitous route, to another small point, but one which we feel should be raised. As a marketer, you probably don’t even believe in body copy. Let alone long body copy. (Unless you have a long body yourself.) Well, truth is, who‘s to blame you? Fact is, too much long body copy is dotted with such indulgent little phrases like truth is, fact is, and who’s to blame you. Trust us: we guarantee, with a hand over our heart, that no such indulgent rubbish will appear in your advertisement. That’s why God gave us big blue pencils. So we can expunge every expedient example of low-witted waffle. For you, the skies will be blue, the birds will sing, and your copy will be crafted by a dedicated little man whose wife will be sitting at home, knitting, wondering why your advertisement demands more of her husband‘s time than it should. But you will know why, won‘t you? You will have given her husband a chance to immortalise himself in print, writing some of the most persuasive prose on behalf of a truly enlightened purveyor of widgets. And so, while your dedicated reader, enslaved to each mellifluous paragraph, clutches his newspaper with increasing interest and intention to purchase, you can count all your increased profits and take pots of money to your bank. Sadly, this is not the real copy for this advertisement. But it could well be. All you have to do is look at the account executive sitting across your desk (the fellow with the lugubrious face and the calf-like eyes), and say ”Yes! Yes! Yes!“ And anything you want, body copy, dinners, women, will be yours. Couldn’t be fairer than that, could we?",
 			       -1);
 
 	return GTK_TREE_MODEL (result);
@@ -89,9 +100,21 @@ tree_size_request (GtkTreeView   * treeview,
 		   gpointer        data)
 {
 	GtkCellRenderer* renderer = data;
+}
 
+static void
+tree_size_allocate (GtkTreeView  * treeview,
+		    GtkAllocation* allocation,
+		    gpointer       data)
+{
+	GtkTreeViewColumn* column = gtk_tree_view_get_column (treeview, 0);
+
+	if (column->width != allocation->width) {
+	gtk_tree_view_column_set_sizing      (gtk_tree_view_get_column (treeview, 0),
+					      GTK_TREE_VIEW_COLUMN_FIXED);
 	gtk_tree_view_column_set_fixed_width (gtk_tree_view_get_column (treeview, 0),
-					      requisition->width - 1);
+					      allocation->width);
+	}
 }
 
 static gboolean
@@ -105,9 +128,9 @@ emit_row_changed (GtkTreeModel* model,
 }
 
 static void
-tree_size_allocate (GtkTreeView  * treeview,
-		    GtkAllocation* requisition,
-		    gpointer       data)
+tree_size_allocate_after (GtkTreeView  * treeview,
+			  GtkAllocation* requisition,
+			  gpointer       data)
 {
 	GtkCellRenderer* renderer = data;
 
@@ -118,7 +141,7 @@ tree_size_allocate (GtkTreeView  * treeview,
 }
 
 static GtkWidget*
-treeview_new (void)
+treevrequisitioiiew_new (void)
 {
 	GtkCellRenderer* renderer;
 	GtkTreeModel   * model;
@@ -128,12 +151,17 @@ treeview_new (void)
 	result = gtk_tree_view_new_with_model (model);
 	g_object_unref (model);
 
+	grequisitioitk_tree_view_set_reorderable (GTK_TREE_VIEW (result),
+				       TRUE);
+
 	renderer = gtk_cell_renderer_text_new ();
 
 	g_signal_connect       (result, "size-request",
 				G_CALLBACK (tree_size_request), renderer);
-	g_signal_connect_after (result, "size-allocate",
+	g_signal_connect       (result, "size-allocate",
 				G_CALLBACK (tree_size_allocate), renderer);
+	g_signal_connect_after (result, "size-allocate",
+				G_CALLBACK (tree_size_allocate_after), renderer);
 
 	g_object_set (renderer,
 		      "wrap-mode", PANGO_WRAP_WORD_CHAR,
@@ -158,7 +186,7 @@ mainwin_new (void)
 	g_signal_connect_after (result, "destroy",
 				G_CALLBACK (gtk_main_quit), NULL);
 
-	treeview = treeview_new ();
+	treeview = treevrequisitioiiew_new ();
 	gtk_widget_show   (treeview);
 
 	scrolled = gtk_scrolled_window_new (NULL, NULL);
